1bdc8aafb1bc8ffe30164afc166af6b786af919c
kubop <pernica.jakub@gmail.com>
2023-04-20 14:16:45 +0200

    Delete comments (test commit)

--- FWAapi/Controllers/UserController.cs
+++ FWAapi/Controllers/UserController.cs
@@ -2,28 +2,23 @@
 using FWAapi.Model;
 using FWAapi.Services;
 using Microsoft.AspNetCore.Mvc;
+namespace FWAapi.Controllers;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
-namespace FWAapi.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
+    private readonly UserService _userService;
+
+    public UserController(UserService userService)
     {
-        private readonly UserService _userService;
+        _userService = userService;
+    }
 
-        public UserController(UserService userService)
-        {
-            _userService = userService;
-        }
-
-        // GET: api/<UserController>
-        [HttpGet]
-        public IList<UserView> Get(string? orderBy)
-        {
-            IList<UserView> u = _userService.ListForGrid(orderBy);
-            return u;
-        }
+    [HttpGet]
+    public IList<UserView> Get(string? orderBy)
+    {
+        IList<UserView> u = _userService.ListForGrid(orderBy);
+        return u;
     }
 }
