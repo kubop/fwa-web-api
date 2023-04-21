136d4d2e07edfa45e315a37edd6e6818996f1fd6
kubop <pernica.jakub@gmail.com>
2023-04-21 08:42:50 +0200

    Add HttpGet method for User edit

--- FWAapi/Controllers/UserController.cs
+++ FWAapi/Controllers/UserController.cs
@@ -9,10 +9,12 @@ namespace FWAapi.Controllers;
 public class UserController : ControllerBase
 {
     private readonly UserService _userService;
+    private readonly AddressService _addressService;
 
-    public UserController(UserService userService)
+    public UserController(UserService userService, AddressService addressService)
     {
         _userService = userService;
+        _addressService = addressService;
     }
 
     [HttpGet]
@@ -26,5 +28,31 @@ public class UserController : ControllerBase
         }
 
         return u;
+    }
+
+    public class UserAddressesModel
+    {
+        public User User { get; set; }
+        public IList<Address> Addresses { get; set; } = new List<Address>();
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Edit(int id)
+    {
+        var user = _userService.GetObjectById(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var addressList = _addressService.GetAllAddresses();
+
+        var model = new UserAddressesModel
+        {
+            User = user,
+            Addresses = addressList
+        };
+
+        return Ok(model);
     }
 }
