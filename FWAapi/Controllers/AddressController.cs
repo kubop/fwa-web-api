fb72bc881d217f5c63ed40aa1df2366ee3e29841
kubop <pernica.jakub@gmail.com>
2023-04-21 08:25:45 +0200

    Add AddressController

---
+++ FWAapi/Controllers/AddressController.cs
@@ -0,0 +1,26 @@
+﻿using FWAapi.Model;
+using FWAapi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FWAapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressController : ControllerBase
+    {
+        private readonly AddressService _addressService;
+
+        public AddressController(AddressService addressService)
+        {
+            _addressService = addressService;
+        }
+
+        [HttpGet]
+        public IList<Address> Get(string? orderBy)
+        {
+            IList<Address> u = _addressService.GetAllAddressesWithCount();
+            return u;
+        }
+    }
+}
